5fce0cd3ac0b43c36561a691df2935cf71bd3378
beauvillain <mbeauvillain@red1-innovation.com>
2016-01-07 14:15:29 +0100

    Init de l'exemple

--- PPDProject/ui.R
+++ PPDProject/ui.R
@@ -1,30 +1,25 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-#
-library(shiny)
-
-shinyUI(fluidPage(
-
+fluidPage(
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
+  titlePanel("Word Cloud"),
+  
   sidebarLayout(
+    # Sidebar with a slider and selection inputs
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      selectInput("selection", "Choose a book:",
+                  choices = books),
+      actionButton("update", "Change"),
+      hr(),
+      sliderInput("freq",
+                  "Minimum Frequency:",
+                  min = 1,  max = 50, value = 15),
+      sliderInput("max",
+                  "Maximum Number of Words:",
+                  min = 1,  max = 300,  value = 100)
     ),
-
-    # Show a plot of the generated distribution
+    
+    # Show Word Cloud
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("plot")
     )
   )
-))
+)
