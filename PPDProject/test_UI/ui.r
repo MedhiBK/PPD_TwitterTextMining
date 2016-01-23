28bdb0a73a50e72b33fbff9b92cde298076e5e98
DamDam <dam.jacquet@gmail.com>
2016-01-23 15:21:17 +0100

    Ajout fichiers d'interface

---
+++ PPDProject/test_UI/ui.r
@@ -0,0 +1,37 @@
+library(shiny)
+
+# Define UI for slider demo application
+shinyUI(pageWithSidebar(
+  
+  
+  #  Application title
+  headerPanel("Parameters"),
+  
+  # Sidebar with sliders that demonstrate various available options
+  sidebarPanel(
+    
+    #Search keywords
+    textInput("keywords", "Keywords :", ""),
+    actionButton("search", "Search"),
+    
+    # Simple integer interval
+    sliderInput("integer", "Max tweets display:", 
+                min=0, max=1000, value=500),
+    
+    
+    dateRangeInput('dateRange',
+                   label = 'Date range',
+                   start = Sys.Date() - 2, end = Sys.Date() + 2
+    ),
+    
+    
+    # Animation with custom interval (in ms) to control speed, plus looping
+    sliderInput("animation", "Looping Animation:", 1, 2000, 1, step = 10, 
+                animate=animationOptions(interval=300, loop=T))
+  ),
+  
+  # Show a table summarizing the values entered
+  mainPanel(
+    tableOutput("values")
+  )
+))
