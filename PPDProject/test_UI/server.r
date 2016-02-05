708d3ff44df230bb0e3c2c1b850f72e9faf48266
SamyHamiche <hamiche.Samy@gmail.com>
2016-02-05 16:36:06 +0100

    Ajout de l'interface de recherche de tweets

--- PPDProject/test_UI/server.r
+++ PPDProject/test_UI/server.r
@@ -1,41 +1,49 @@
 library(shiny)
-library(datasets)
-source("tweet_extract.r")
+library(twitteR)
+library(wordcloud)
+library(tm)
 
-# Define server logic required to summarize and view the selected dataset
-shinyServer(function(input, output) {
+consumerKey = "fIZMJKnLQ2RuGQQEEizjy2GzA"   
+consumerSecret = "fEnlXSOzuMWUAcg9x9g2AAWjT3mxMQ4l4ZbeLnfcH7S4IEIV4L"
+accessToken = "4835171254-cMJUm81pHUMRNZCYd0xX0q8ZHzPQqDeqBaoCkM4"
+accessSecret = "NKhbXctNNEbdKGulVtb6hrydq3VXTriZw2kYMKgp7PJZL"
+my_oauth <- setup_twitter_oauth(consumer_key = consumerKey, consumer_secret = consumerSecret,
+                                access_token = accessToken, access_secret = accessSecret)
+1
+
+
+
+shinyServer(function(input,output){
   
-  # Return the requested dataset
-  datasetInput <- reactive({
-    switch(input$dataset,
-           "rock" = rock,
-           "pressure" = pressure,
-           "cars" = cars)
+  rawData <- (function(){
+    tweets <- searchTwitter(input$term, n=input$cant,lang=input$lang)
+    twListToDF(tweets)
   })
   
-  #Recherche des tweets
-  library(twitteR)
-  api_key <- "fIZMJKnLQ2RuGQQEEizjy2GzA" # From dev.twitter.com
-  api_secret <- "fEnlXSOzuMWUAcg9x9g2AAWjT3mxMQ4l4ZbeLnfcH7S4IEIV4L" # From dev.twitter.com
-  token <- "4835171254-cMJUm81pHUMRNZCYd0xX0q8ZHzPQqDeqBaoCkM4" # From dev.twitter.com
-  token_secret <- "NKhbXctNNEbdKGulVtb6hrydq3VXTriZw2kYMKgp7PJZL" # From dev.twitter.com
-  setup_twitter_oauth(api_key, api_secret, token, token_secret)
-  tweets <- searchTwitter("Mascherano", n=100, lang="en", since="2016-01-20")
-  tweets.df <- twListToDF(tweets)
-  
-  # Generate a summary of the dataset
-  output$summary <- renderPrint({
-    dataset <- datasetInput()
-    summary(dataset)
+  output$table <- renderTable(function(){
+    head(rawData()[1],n=input$cant)
   })
   
-  #Render ?
-  output$tweets_list = renderDataTable({
-    tweets.df
-    })
-  
-  # Show the first "n" observations
-  output$view <- renderTable({
-    head(datasetInput(), n = input$obs)
+  output$wordcl <- renderPlot(function(){
+    tw.text <- enc2native(rawData()$text)
+    tw.text <- tolower(tw.text)
+    tw.text <- removeWords(tw.text,c(stopwords(input$lang),"rt"))
+    tw.text <- removePunctuation(tw.text, TRUE)
+    tw.text <- unlist(strsplit(tw.text,""))
+    
+    word <- sort(table(tw.text),TRUE)
+    wordc <- head(word,n=15)
+    
+    
+    
+    wordcloud(names(wordc),wordc,random.color = TRUE,colors = rainbow(10),scale = c(15,2))
   })
+  
+  output$download <- downloadHandler(filename = function() {paste(input$term, '.csv', sep='')},
+                                     content = function(file){
+                                       write.csv(rawData(), file)
+                                     }
+  )
+  
+  
 })
--- PPDProject/test_UI/ui.r
+++ PPDProject/test_UI/ui.r
@@ -1,38 +1,22 @@
 library(shiny)
+library(twitteR)
 
-# Define UI for slider demo application
+
 shinyUI(pageWithSidebar(
+  headerPanel("Analyse de tweets"),
+  
+  sidebarPanel(textInput("term", "Entrer un mot", "twitter"),
+               sliderInput("cant", "Choisir un nombre de tweets",min=5,max=100, value = 5),
+               radioButtons("lang", "Choisir une langue", c(
+                 "Français"="fr",
+                 "English"="en")),
+               submitButton(text = "Recherche"),
+               
+               downloadButton("download", "Télécharger")),
   
   
-  #  Application title
-  headerPanel("Parameters"),
-  
-  # Sidebar with sliders that demonstrate various available options
-  sidebarPanel(
-    
-    #Search keywords
-    textInput("keywords", "Keywords :", ""),
-    actionButton("search", "Search"),
-    
-    # Simple integer interval
-    sliderInput("integer", "Max tweets display:", 
-                min=0, max=1000, value=500),
-    
-    
-    dateRangeInput('dateRange',
-                   label = 'Date range',
-                   start = Sys.Date() - 2, end = Sys.Date() + 2
-    ),
-    
-    
-    # Animation with custom interval (in ms) to control speed, plus looping
-    sliderInput("animation", "Looping Animation:", 1, 2000, 1, step = 10, 
-                animate=animationOptions(interval=300, loop=T))
-  ),
-  
-  # Show a table summarizing the values enteredYy
   mainPanel(
-    h2('List of tweets found'),
-    dataTableOutput('tweets_list')
-  )
+    h4("Liste des tweets"),
+    tableOutput("table"),
+    plotOutput("wordcl"))
 ))
