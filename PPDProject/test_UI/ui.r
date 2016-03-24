a75a2f9219f6ec75ae64147c36d06460b191954b
DamDam <dam.jacquet@gmail.com>
2016-03-24 11:33:37 +0100

    ListBox pour la liste des langues. Ajout dans l'interface d'une recherche daté (recherche a validé ou pas).

--- PPDProject/test_UI/ui.r
+++ PPDProject/test_UI/ui.r
@@ -7,12 +7,22 @@ shinyUI(pageWithSidebar(
   
   sidebarPanel(textInput("term", "Entrer un mot", "twitter"),
                sliderInput("cant", "Choisir un nombre de tweets",min=5,max=100, value = 5),
-               radioButtons("lang", "Choisir une langue", c(
-                 "Français"="fr",
-                 "English"="en")),
+#                radioButtons("lang", "Choisir une langue", c(
+#                  "Français"="fr",
+#                  "English"="en")),
+               selectInput("lang", label = "Langue", 
+                           choices = list("Français" = "fr", "English" = "en"), 
+                           selected = 1),
+
+               dateRangeInput('dateRange',
+               label = 'Tweeté entre le',
+               start = Sys.Date() - 2, end = Sys.Date() + 2,
+               separator = " et le ", format = "dd/mm/yy", language = "fr"
+               ),
                submitButton(text = "Recherche"),
                
                downloadButton("download", "Télécharger")),
+          
   
   
   mainPanel(
