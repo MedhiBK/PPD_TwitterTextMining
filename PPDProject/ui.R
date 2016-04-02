eeb96ad32595a1c156558a39de12350cbb875634
DamDam <dam.jacquet@gmail.com>
2016-04-02 17:29:58 +0200

    Nettoyage fichiers

--- PPDProject/ui.R
+++ PPDProject/ui.R
@@ -1,25 +1,38 @@
-fluidPage(
-  # Application title
-  titlePanel("Word Cloud"),
+library(shiny)
+library(twitteR)
+library(shinyBS)
+
+
+shinyUI(pageWithSidebar(
+  headerPanel("Analyse de tweets"),
   
-  sidebarLayout(
-    # Sidebar with a slider and selection inputs
-    sidebarPanel(
-      selectInput("selection", "Choose a book:",
-                  choices = books),
-      actionButton("update", "Change"),
-      hr(),
-      sliderInput("freq",
-                  "Minimum Frequency:",
-                  min = 1,  max = 50, value = 15),
-      sliderInput("max",
-                  "Maximum Number of Words:",
-                  min = 1,  max = 300,  value = 100)
-    ),
-    
-    # Show Word Cloud
-    mainPanel(
-      plotOutput("plot")
-    )
-  )
-)
+  sidebarPanel(textInput("term", "Entrer un mot", "Data Mining"),
+               sliderInput("cant", "Choisir un nombre de tweets",min=5,max=100, value = 20),
+#                radioButtons("lang", "Choisir une langue", c(
+#                  "Français"="fr",
+#                  "English"="en")),
+                sliderInput("freq", "Fréquence d'apparition du mot",min=1,max=30, value = 1),
+               selectInput("lang", label = "Langue", 
+                           choices = list("Français" = "fr", "English" = "en"), 
+                           selected = 1),
+
+               dateRangeInput('dateRange',
+               label = 'Tweeté entre le',
+               start = Sys.Date() - 30, end = Sys.Date() ,
+               separator = " et le ", format = "dd/mm/yy", language = "fr"
+               ),
+              bsTooltip("term", "Pour exclure un mot ajoutez un tiret - devant ce dernier",
+              "right", options = list(container = "body")),
+               submitButton(text = "Recherche"),
+               
+               downloadButton("download", "Télécharger")),
+          
+  
+  
+  mainPanel(
+    h4("Top 5 des mots"),
+    tableOutput("table"),
+    plotOutput("wordcl"))
+    #h4("Liste des tweets"),
+    #tableOutput("table"))
+))
