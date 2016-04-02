18b3a09b94ca170f755583640eaf84753886f85d
Yanis <yanis.cherifi@gmail.com>
2016-04-02 17:07:40 +0200

    add table best five word

--- PPDProject/test_UI/server.r
+++ PPDProject/test_UI/server.r
@@ -22,6 +22,26 @@ shinyServer(function(input,output){
     head(rawData()[1],n=input$cant)
   })
   
+  output$tableau <- renderTable(function(){
+    tw.text <- enc2native(rawData()$text)
+    tw.text <- gsub(" ?(f|ht)(tp)(s?)(://)(.*)[.|/](.*)", "", tw.text)
+    tw.text <- gsub(" ?(f|ht)(tp)(s?)(.*)", "", tw.text)
+    tw.text <- gsub(" (.*)[.0-9](.*)", "", tw.text)
+    tw.text <- tolower(tw.text)
+    tw.text <- removeWords(tw.text,c(stopwords(input$lang),"rt"))
+    tw.text <- removePunctuation(tw.text, TRUE)
+    tw.text <- unlist(strsplit(tw.text," "))
+    word <- sort(table(tw.text),TRUE)
+    wordc <- head(word,n=6)
+    vecteur1 <- names(wordc)
+    vecteur2 <- word
+    vecteur3 = c(vecteur1,vecteur2)
+    matrice = matrix(data = vecteur3,nrow=2,ncol=6,byrow=TRUE,dimnames = list(c("Mot","Fréquence")))
+    print(matrice)
+    matrice = matrice[,-1]
+    
+  })
+  
   output$wordcl <- renderPlot(function(){
     tw.text <- enc2native(rawData()$text)
     tw.text <- gsub(" ?(f|ht)(tp)(s?)(://)(.*)[.|/](.*)", "", tw.text)
